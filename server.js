1b7ebfd260ca8da85fd9e0cb5f8878b524f12d75
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-09-16 18:59:22 -0400

    reading db.json

--- server.js
+++ server.js
@@ -19,10 +19,20 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// default to index.html
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/api/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'db.json'))
 );
+
+app.post('/api/notes', (req, res) =>
+    fs.readFile(path.join(__dirname, 'db.json'), 'utf-8', function(error, response) {
+        if (error) {
+            console.log(error);
+        }
+    })
+)
+
+// default to index.html
+app.get('*', (req, res) => res.redirect('/public/index.html'));
 
 app.listen(PORT, function() {
     console.log(`Server is listening on PORT: http://localhost:${PORT}`);

4705361021fab0ab129086cf26aae6f2f4ec0082
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-09-16 18:30:00 -0400

    file paths to index and notes htmls

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
+const PORT = 3001;
 const app = express();
 
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// grabbing from homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+// route to notes.html page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// default to index.html
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 app.listen(PORT, function() {
-    console.log(`Server is listening on PORT: http://localhost${PORT}`);
+    console.log(`Server is listening on PORT: http://localhost:${PORT}`);
   });

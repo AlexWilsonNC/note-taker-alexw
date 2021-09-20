9040bb84e31c568380573d4007b6ce5b49088385
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-09-20 19:46:12 -0400

    app working everywhere but save button

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = 3001;
+const PORT = 3009;
 const app = express();
 
 app.use(express.json());
@@ -16,24 +16,36 @@ app.get('/', (req, res) =>
 
 // route to notes.html page
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'db.json'))
+    res.sendFile(path.join(__dirname, './db/db.json'))
 );
 
 app.post('/api/notes', (req, res) =>
-    fs.readFile(path.join(__dirname, 'db.json'), 'utf-8', function(error, response) {
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', function(error, res) {
         if (error) {
             console.log(error);
         }
+        const notes = JSON.parse(res);
+        const noteRequest = req.body;
+        const newNoteId = notes.length + 1;
+        const newNote = {
+            title: noteRequest.title,
+            text: noteRequest.text,
+            id: newNoteId
+        };
+        notes.push(newNote), res.json(newNote);
+        fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes, null, 2), function(error) {
+            if (error) throw error;
+        })
     })
-)
+);
 
 // default to index.html
-app.get('*', (req, res) => res.redirect('/public/index.html'));
+app.get('*', (req, res) => res.redirect('public/index.html'));
 
 app.listen(PORT, function() {
-    console.log(`Server is listening on PORT: http://localhost:${PORT}`);
+    console.log(`Server is listening on PORT: http://localhost:${PORT} 📝`);
   });
